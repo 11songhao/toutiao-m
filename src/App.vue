c631803929b3abe8c43a6489e2f97e7c6a4d8ed7
songhao <1146987523@qq.com>
2022-07-03 20:41:46 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+ <div>
+
+ 
+ </div>
+</template>
+<script>
+export default {
+data(){
+ 
+},
+ props:{ 
+obj: 
+ {
+ type: Object,
+ default: () => { } 
+} 
+}, 
+ components: {
+ 
+ },
+ methods: {
+ 
+ },
+ created () {
+ 
+ },
+ updated () {
+ 
+ },
+ mounted () {
+  
+ },
+ filters: {
+ 
+ },
+ computed: {
+ 
+ },
+ watch: {
+ 
+ },
+}
+</script>
+<style lang="less" scoped>
+
+ 
+</style>
